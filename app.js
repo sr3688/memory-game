e9736aa5f36304a154a26878f708964ad013dc82
Thanasan Kumdee <6200313@kvis.ac.th>
2021-10-01 15:45:18 +0700

    random img sequence

--- app.js
+++ app.js
@@ -1,9 +1,77 @@
-const card = document.querySelectorAll(".card-inner");
+const cardContainer = document.getElementById("card-container");
 const moves = document.querySelector(".moves-count");
 
 var flipCounter = 0;
 var movesCounter = 0;
-let remainingCards = card.length;
+const frontImg = "./img/Dragon_Ball_Cast.png";
+let characters = [
+  {
+    name: "magin-bu",
+    img: "./img/magin-bu.jpeg",
+  },
+  {
+    name: "goku",
+    img: "./img/goku.png",
+  },
+  {
+    name: "beerus",
+    img: "./img/beerus.jpeg",
+  },
+  {
+    name: "vegeta",
+    img: "./img/vegeta.jpeg",
+  },
+  {
+    name: "zeno",
+    img: "./img/zeno.png",
+  },
+  {
+    name: "gohan",
+    img: "./img/gohan.jpeg",
+  },
+];
+
+let remainingCards = characters.length * 2;
+
+function initializeGame() {
+  let availableCharacterCards = [];
+
+  characters.forEach((character) => {
+    availableCharacterCards.push(character);
+    availableCharacterCards.push(character);
+  });
+
+  for (let index = 0; index < characters.length * 2; index++) {
+    let idx = Math.floor(availableCharacterCards.length * Math.random());
+    let currentCharacter = availableCharacterCards[idx];
+    availableCharacterCards.splice(idx, 1);
+
+    let cardItem = document.createElement("div");
+    cardItem.className = "card";
+
+    let cardInner = document.createElement("div");
+    cardInner.className = "card-inner";
+
+    cardInner.setAttribute("_title", currentCharacter.name);
+
+    let cardFront = document.createElement("div");
+    cardFront.className = "card-face card-face-front";
+    let cardFrontImg = document.createElement("img");
+    cardFrontImg.src = frontImg;
+    cardFront.appendChild(cardFrontImg);
+
+    let cardBack = document.createElement("div");
+    cardBack.className = "card-face card-face-back";
+    let cardBackImg = document.createElement("img");
+    cardBackImg.src = currentCharacter.img;
+    cardBack.appendChild(cardBackImg);
+
+    cardInner.appendChild(cardFront);
+    cardInner.appendChild(cardBack);
+    cardItem.appendChild(cardInner);
+    cardContainer.appendChild(cardItem);
+  }
+}
 
 function flipBackAllAndVisibility() {
   setTimeout(function () {
@@ -24,7 +92,8 @@ function flipBackAllAndVisibility() {
                   moves.innerHTML = movesCounter;
                   remainingCards -= 2;
                   if (remainingCards == 0) {
-                    console.log("yy");
+                    console.log("end");
+                    // Show result page
                   }
                 } else {
                   card[index].classList.toggle("is-flipped");
@@ -41,6 +110,9 @@ function flipBackAllAndVisibility() {
     }
   }, 1000);
 }
+
+initializeGame();
+const card = document.querySelectorAll(".card-inner");
 
 for (let item = 0; item < card.length; item++) {
   card[item].addEventListener("click", () => {
